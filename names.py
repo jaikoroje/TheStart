8f8e4a9f9147225103c130af31df3a889f06e9bf
jaikoroje <JAikoroje@Cs-MacBook-Pro.local>
2023-12-05 14:57:25 +0100

    fix the function to calculate mean

---
+++ names.py
@@ -0,0 +1,15 @@
+def cal_mean(data):
+    total_sum = 0
+    
+    for i in data:
+        total_sum = i + total_sum
+        
+    n = len(data)
+
+    mean = total_sum/n
+    return (total_sum , mean)
+
+ages = [4,3,7,6,5,2,8]
+x = cal_mean(ages)
+
+print(x)
